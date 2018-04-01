86afcb37275a47433f6feca3c85fc8cb361ef470
kwangil <kylekwp22@gmail.com>
2018-04-01 12:57:53 -0400

    added sqlite support, redirect output to the txt file

--- diff.py
+++ diff.py
@@ -2,24 +2,37 @@ import difflib
 import sys
 import fileinput
 import os
+import contextlib
+
 
 #diff.py
 #to find any difference between html sources
+#written by kwp
 
 def main():
     if len(sys.argv) >=3:
     	folder1 = sys.argv[1]
     	folder2 = sys.argv[2]
     else:
-	print 'Usage: python diff.py -folder1 -folder2'
+	print 'Usage: python diff.py name_of_folder1 name_of_folder2\r'
+	print 'Usage: python diff.py aaa.sqlite bbb.sqlite\r'
 	return
 
-    concat_sources(folder1)
-    concat_sources(folder2)
-    _diff(os.getcwd()+'/'+str(folder1)+'/'+str(folder1)+'.html',os.getcwd()+'/'+str(folder2)+'/'+str(folder2)+'.html')
+    if '.sqlite' in sys.argv[1]:
+	print 'Comparing two sqlite files...'
+	_diff(os.getcwd()+'/'+str(folder1),os.getcwd()+'/'+str(folder2))
+    else:
+    	concat_sources(folder1)
+    	concat_sources(folder2)
+    	_diff(os.getcwd()+'/'+str(folder1)+'/'+str(folder1)+'.html',os.getcwd()+'/'+str(folder2)+'/'+str(folder2)+'.html')
     
 
 def concat_sources(folder):
+
+	try:
+		os.remove(os.getcwd()+'/'+str(folder)+'/'+str(folder)+'.html')
+	except OSError:
+		pass
 	
 	path = os.path.join(os.getcwd(),folder)
 	html_files= []
@@ -28,7 +41,9 @@ def concat_sources(folder):
 			if file.endswith(".html"):
 				html_files.append(os.path.join(root,file))
 		break
-	print (html_files)
+	#print (html_files)
+
+	
 
 	temp_file = open(os.getcwd()+'/'+str(folder)+'/'+str(folder)+'.html','w')
 	print(os.getcwd()+'/'+str(folder)+'/'+str(folder)+'.html'+ ' has been created')
@@ -43,7 +58,11 @@ def concat_sources(folder):
     
 def _diff(file1 , file2):
 	diff = difflib.ndiff(open(file1).readlines(),open(file2).readlines())
-	print ''.join(diff),
+	with open('result.txt','w') as f:
+		f.write(''.join(diff),)
+	
+	print('result.txt has been created')
+	
 	return
 
 
